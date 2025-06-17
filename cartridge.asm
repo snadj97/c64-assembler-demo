8ac5300d18fbb502cce35436f39400e6d2605df5
Simon Nadj <simonnadj@hotmail.com>
2025-06-17 20:35:31 +0200

    Re-format source file

--- cartridge.asm
+++ cartridge.asm
@@ -18,77 +18,70 @@ CURSOR_PHASE = $00cf
 GETIN        = $ffe4
 
 
-coldstart:
-    SEI         ;Set Interrupt
-    STX $d016   ;Store register x (but why?) - it seems 0xFF is stored in X from the start of the Commodore64. This is stored in $d016 for initialization.
-    JSR $fda3   ;Prepare IRQ
-    JSR $fd50   ;Init memory. Rewrite this routine to speed up boot process
-    JSR $fd15   ;Init I/O
-    JSR $ff5b;  ;Init video
-    CLI         ;Clear interrupt
 
-warmstart:
-; Setup start {
-    ; Background and border color to black
-    LDA #0
-    STA BACKGROUND
-    STA BORDER
-
-    ; All characters to green
-    JSR charcolor ; For storing characters in the char matrix
-    LDA #$05
-    STA CURSOR_COLOR     ; Change cursor color to green
+coldstart:  SEI         ;Set Interrupt
+            STX $d016   ;Store register x (but why?) - it seems 0xFF is stored in X from the start of the Commodore64. This is stored in $d016 for initialization.
+            JSR $fda3   ;Prepare IRQ
+            JSR $fd50   ;Init memory. Rewrite this routine to speed up boot process
+            JSR $fd15   ;Init I/O
+            JSR $ff5b;  ;Init video
+            CLI         ;Clear interrupt
 
 
-    ; ; Convert a byte value to a hex string (2 chars)...
-    ; LDA #127
-    ; JSR bytetohex
 
-    ; ; ... and print them to the screen
-    ; TYA
-    ; JSR CHROUT
-    ; TXA
-    ; JSR CHROUT
 
-    ; Print 'HELLO WORLD' in the middle of the screen.
-    JSR hello_world
+warmstart:  LDA #0      ; Background and border color to black
+            STA BACKGROUND
+            STA BORDER
 
-    ; Set cursor position
-    CLC
-    LDX #6
-    LDY #0
-    JSR PLOT
+            ; All characters to green
+            JSR charcolor ; For storing characters in the char matrix
+            LDA #$05
+            STA CURSOR_COLOR     ; Change cursor color to green
 
-    LDA #0
-    STA CURSOR_SHOW
 
-; } Setup end
+            ; ; Convert a byte value to a hex string (2 chars)...
+            ; LDA #127
+            ; JSR bytetohex
+
+            ; ; ... and print them to the screen
+            ; TYA
+            ; JSR CHROUT
+            ; TXA
+            ; JSR CHROUT
+
+            ; Print 'HELLO WORLD' in the middle of the screen.
+            JSR hello_world
+
+            ; Set cursor position
+            CLC
+            LDX #6
+            LDY #0
+            JSR PLOT
+
+            LDA #0
+            STA CURSOR_SHOW
 
 main_loop:
 ; Loop start
 
-wait_char:
-    JSR GETIN
-    CMP #0
-    BEQ wait_char
+wait_char:  JSR GETIN
+            CMP #0
+            BEQ wait_char
 
-    CMP #$0d
-    BNE print_char
+            CMP #$0d
+            BNE print_char
 
-wait_cursor_off:
-    LDA CURSOR_PHASE
-    BNE wait_cursor_off
-    LDA #$0d
+wait_cursor_off:    LDA CURSOR_PHASE
+                    BNE wait_cursor_off
+                    LDA #$0d
 
-print_char:
-    JSR CHROUT
-
+print_char: JSR CHROUT
 
 ; Loop end
     JMP main_loop
 
-hello_world:
-    SUBROUTINE
+hello_world:    SUBROUTINE
     ; Set cursor position
     CLC
     LDX #2
@@ -97,89 +90,83 @@ hello_world:
 
     ; Loop through constant, zero-terminated string
     LDX #0
-.loop:
-    LDA hello_world_str,X
-    JSR CHROUT
-    INX
-    CMP #0
-    BNE .loop
 
-    LDA #'!
-    JSR CHROUT
+.loop:  LDA hello_world_str,X
+        JSR CHROUT
+        INX
+        CMP #0
+        BNE .loop
 
-    RTS
+        LDA #'!
+        JSR CHROUT
 
-charcolor:
+        RTS
+
+
 ; Set character color to green for all screen characters
 ; This is usefule, if characters are placed manually. Otherwise,
 ; consider setting the cursor text color setting $0286 and outputting
 ; characters to screen using JSR $ffd2
-    SUBROUTINE
-
-; Load start address for setting character color ($d800)
+charcolor:    SUBROUTINE
+    ; Load start address for setting character color ($d800)
     LDA #$00
     STA $08     ; 16-bit value, low
     LDA #$d8
     STA $09     ; 16-bit value, high
     LDX #0      ; Set X to 0 (no address offset)
 
-.loop
+.loop   LDA #$05    ; Green
+        STA ($08,X) ; Store A in address [$01, $00] -> $d800
 
-    LDA #$05    ; Green
-    STA ($08,X) ; Store A in address [$01, $00] -> $d800
+        CLC
+        LDA $08
+        ADC #1
+        STA $08
 
-    CLC
-    LDA $08
-    ADC #1
-    STA $08
+        BVS .inchigh ; If overflow, increment high byte
 
-    BVS .inchigh ; If overflow, increment high byte
-
-.comp
 ; Compare to highest color address ($dbe7) and loop until reached
-    LDA $08
-    CMP #$e7
-    BNE .loop
+.comp   LDA $08
+        CMP #$e7
+        BNE .loop
 
-    LDA $09
-    CMP #$db
-    BNE .loop
+        LDA $09
+        CMP #$db
+        BNE .loop
 
-    RTS
+        RTS
 
-.inchigh:
-    CLC
-    LDA $09
-    ADC #1
-    STA $09
+.inchigh:   CLC
+            LDA $09
+            ADC #1
+            STA $09
 
-    JMP .comp
+            JMP .comp
 
-bytetohex:
+
 ; in:   A  (byte)
 ; out:  X  (hi-nibble)
 ;       Y  (lo-nibble)
-    PHA     ; Push A to stack. A preserves value
-    LSR     ; Shift A right 4 times (127: 0x7F -> 0111 1111 >> 0000 0111)
-    LSR
-    LSR
-    LSR
-    TAX             ; Transfer A to X -> X = 0000 0111
-    LDA hexits,X    ; Look up value in character table: 0x07 -> '7' and load into A
-    STA tmp         ; Store A in variable tmp (in RAM, see SEG.U variables)
+bytetohex:  PHA     ; Push A to stack. A preserves value
+            LSR     ; Shift A right 4 times (127: 0x7F -> 0111 1111 >> 0000 0111)
+            LSR
+            LSR
+            LSR
+            TAX             ; Transfer A to X -> X = 0000 0111
+            LDA hexits,X    ; Look up value in character table: 0x07 -> '7' and load into A
+            STA tmp         ; Store A in variable tmp (in RAM, see SEG.U variables)
 
-    PLA             ; Pull original A value into A (0x7F)
-    AND #%00001111  ; AND A to get LSBs (0x7F & 0x0F -> 0x0F: 0000 1111)
-    TAX             ; Transfer A to X -> X = 0000 1111
-    LDA hexits,X    ; Look up value in character table: 0x0F -> 'f' and load into A
-    TAX             ; Transfer A to X (hi-nibble output)
-    LDY tmp         ; Load Y with value stored in 'tmp' (low-nibble output)
-    RTS
-
+            PLA             ; Pull original A value into A (0x7F)
+            AND #%00001111  ; AND A to get LSBs (0x7F & 0x0F -> 0x0F: 0000 1111)
+            TAX             ; Transfer A to X -> X = 0000 1111
+            LDA hexits,X    ; Look up value in character table: 0x0F -> 'f' and load into A
+            TAX             ; Transfer A to X (hi-nibble output)
+            LDY tmp         ; Load Y with value stored in 'tmp' (low-nibble output)
+            RTS
 
 ; Constants
-hexits: BYTE "0123456789ABCDEF"    ; IMPORTANT that characters here are capital letters!
-hello_world_str: BYTE "HELLO WORLD",0
+hexits:             BYTE "0123456789ABCDEF"    ; IMPORTANT that characters here are capital letters!
+hello_world_str:    BYTE "HELLO WORLD",0
 
 ; EOF - Fill up to -$9fff (or $bfff if 16K)
     ORG $9fff
